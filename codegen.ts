b77bfbd93eec7a48e87d23f337d497eab215b08d
Cyfroni <karolplg@gmail.com>
2023-01-12 17:53:55 +0100

    gql added

---
+++ codegen.ts
@@ -0,0 +1,16 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const config: CodegenConfig = {
+  schema: process.env.GRAPHQL_API_URL,
+  documents: ["app/**/*.tsx"],
+  generates: {
+    "src/gql/": {
+      preset: "client",
+      plugins: [],
+    },
+  },
+  errorsOnly: true,
+};
+export default config;
